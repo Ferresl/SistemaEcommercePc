6abdc22c1a94845f70bb58191bf8b5ccf7effc8a
FerresI <jua.ferreira@duocuc.cl>
2026-05-17 21:48:36 -0400

    Arreglo mensajes y validaciones de autenticacion

--- Auth-User/Auth-User/src/main/java/DuocucEcommerce/Auth_User/Dto/LoginRequestDTO.java
+++ Auth-User/Auth-User/src/main/java/DuocucEcommerce/Auth_User/Dto/LoginRequestDTO.java
@@ -12,9 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LoginRequestDTO {
+
     @NotBlank(message = "El email es obligatorio")
-    @Email(message = "El email no es valido")
+    @Email(message = "El email no es valido, debe contener un @")
     private String email;
-    @NotBlank(message = "La password es obligatoria")
+
+    @NotBlank(message = "La password es obligatoria no puede quedar en blanco")
     private String password;
 }
--- Auth-User/Auth-User/src/main/java/DuocucEcommerce/Auth_User/Dto/RegistroRequestDTO.java
+++ Auth-User/Auth-User/src/main/java/DuocucEcommerce/Auth_User/Dto/RegistroRequestDTO.java
@@ -16,22 +16,22 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegistroRequestDTO {
 
-    @NotBlank(message = "El nombre es obligatorio")
+    @NotBlank(message = "El nombre es obligatorio , no puede quedar en blanco")
     @Size(min = 2, max = 60)
     private String nombre;
 
-    @NotBlank(message = "El apellido es obligatorio")
-    @Size(min = 2, max = 60)
+    @NotBlank(message = "El apellido es obligatorio , no puede quedar en blanco")
+    @Size(min = 2, max = 60, message = "debe contener minimo 2 letras y maximo 60")
     private String apellido;
 
-    @NotBlank(message = "El email es obligatorio")
-    @Email(message = "El email no es valido")
+    @NotBlank(message = "El email es obligatorio, no puede quedar en blanco")
+    @Email(message = "El email no es valido, debe contener @")
     private String email;
 
-    @NotBlank(message = "La password es obligatoria")
-    @Size(min = 6, max = 40)
-    
+    @NotBlank(message = "La password es obligatoria, no puede quedar en blanco")
+    @Size(min = 6, max = 40 , message ="Debe contener minimo 6 caracteres y maximo 40")
     private String password;
+    
     @NotBlank(message = "La confirmacion es obligatoria")
     private String confirmarPassword;
 
