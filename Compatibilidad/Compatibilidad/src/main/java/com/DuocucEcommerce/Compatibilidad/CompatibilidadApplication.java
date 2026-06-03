c096ce3ef379bfcbb6e01aa336d74e0fb653bf59
unknown <SSDD@PNAWSA04LC41527.duoc.acad>
2026-06-03 12:37:40 -0400

    Actualizamos properties por yml para conectar con el servidor eureka

--- Compatibilidad/Compatibilidad/src/main/java/com/DuocucEcommerce/Compatibilidad/CompatibilidadApplication.java
+++ Compatibilidad/Compatibilidad/src/main/java/com/DuocucEcommerce/Compatibilidad/CompatibilidadApplication.java
@@ -2,7 +2,8 @@ package com.DuocucEcommerce.Compatibilidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompatibilidadApplication {
 
